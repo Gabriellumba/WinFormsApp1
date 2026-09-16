124c384f5e164e5af7b63a1ded118cefb5841b5d
Gabriellumba <st10499215@rcconnect.edu.za>
2026-09-16 16:34:20 +0200

    Update Form1 layout and add new controls

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsApp1
+namespace WinFormsApp1
 {
     partial class Form1
     {
@@ -30,14 +30,18 @@
         {
             button2 = new Button();
             pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button2
             // 
-            button2.Location = new Point(338, 230);
+            button2.Location = new Point(567, 334);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(101, 37);
             button2.TabIndex = 1;
             button2.Text = "Start";
             button2.UseVisualStyleBackColor = true;
@@ -45,17 +49,58 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(12, 24);
+            pictureBox1.Location = new Point(28, 24);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(177, 128);
+            pictureBox1.Size = new Size(133, 140);
             pictureBox1.TabIndex = 2;
             pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(114, 334);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 37);
+            button1.TabIndex = 3;
+            button1.Text = "clear";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(318, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(153, 15);
+            label1.TabIndex = 4;
+            label1.Text = "CYBERSECURITY CHAT BOT";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(227, 202);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(375, 57);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(227, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(375, 131);
+            label2.TabIndex = 6;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Controls.Add(pictureBox1);
             Controls.Add(button2);
             Name = "Form1";
@@ -63,10 +108,15 @@
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private Button button2;
         private PictureBox pictureBox1;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
     }
 }
